73ec164eaac030691e575b5e4d0878f3b6b0adb1
sallescosta <newcapital.in@gmail.com>
2024-11-04 20:56:51 +0100

    feat: create data chat json

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { HeaderElement } from "./components/Header"
 import { FeedPage } from "./pages/FeedPage/FeedPage"
 import { FriendsPage } from "./pages/FriendsPage/FriendsPage"
 import { ChatPage } from "./pages/ChatPage/ChatPage"
+import chat from "./helpers/chat.json"
 
 const contentContainer = document.createElement("div")
 contentContainer.setAttribute("data-js", "contentContainer")
@@ -14,25 +15,25 @@ export type ChatPageContentProps = {
 
 type RenderPageProps = {
   page: "feed" | "chat" | "friends"
-  content?: ChatPageContentProps
+  userId?: string
 }
 
-export const renderPage = ({ page, content }: RenderPageProps) => {
+export const renderPage = ({ page, userId }: RenderPageProps) => {
   contentContainer.innerHTML = ""
 
-  const chat = content ? ChatPage(content) : null
+  const id = userId ?? chat.conversations[0].id
 
   const component =
     {
       feed: FeedPage,
       friends: FriendsPage,
-      chat,
+      chat: ChatPage(id),
     }[page] || FeedPage
 
   contentContainer.appendChild(component)
 }
 
-renderPage({ page: "friends" })
+renderPage({ page: "chat" })
 
 const headerButtons = HeaderElement.querySelectorAll('[data-js="nav-btn"]')
 headerButtons.forEach((button) => {
--- src/pages/ChatPage/ChatPage.ts
+++ src/pages/ChatPage/ChatPage.ts
@@ -1,17 +1,71 @@
 import { dc } from "../../helpers/helpers"
-import { ChatPageContentProps } from "../../main"
+import chat from "../../helpers/chat.json"
 
-export const ChatPage = ({ firstName, lastName }: ChatPageContentProps) => {
-  const a = dc("b")
-  a.textContent = firstName
+export type Message = {
+  timestamp: string
+  sender: string
+  content: string
+}
 
-  const b = dc("b")
-  b.textContent = lastName
+export type Contact = {
+  name: string
+  profilePicture: string
+}
+
+export type ConversationSummary = {
+  contact: Contact
+  lastMessage: Message
+}
+
+export type ConversationDetail = {
+  contact: Contact
+  messages: Message[]
+}
+
+export type ChatData = {
+  conversations: ConversationSummary[]
+}
+
+export const ChatPage = (id: string) => {
+  const selected = chat.conversations.find((c) => c.id === id)
+
+  const List = () => {
+    const list = dc("container")
+    list.setAttribute("data-js", "chat-list")
+    list.classList.add("list")
+
+    if (selected) {
+      console.log("selected :", selected)
+      const a = dc("b")
+      a.textContent = selected?.messages[0].content
+      list.appendChild(a)
+    }
+
+    return list
+  }
+
+  const Chat = () => {
+    const chat = dc("container")
+    chat.setAttribute("data-js", "chat-messages")
+    chat.classList.add("chat")
+
+    if (selected) {
+      const b = dc("b")
+      b.textContent = selected.contact.name
+      chat.appendChild(b)
+    }
+
+    return chat
+  }
+
+  const container = dc("container")
+  container.classList.add("container")
+  container.appendChild(List())
+  container.appendChild(Chat())
 
   const chatPage = dc("div")
   chatPage.setAttribute("data-js", "chat-page")
-  chatPage.appendChild(a)
-  chatPage.appendChild(b)
+  chatPage.appendChild(container)
 
   return chatPage
 }
